5d672112495aef3aa0a8cf7e9b0828efcaffaa42
Diegow7 <diegorafaelariassarango7@gmail.com>
2024-02-07 22:03:59 -0500

    modification login and elimination completarCompra

--- frontend/js/completarCompra.js
+++ frontend/js/completarCompra.js
@@ -1,27 +1,22 @@
-// Get all tabs
-var tabs = document.querySelectorAll('.tab');
+// loginLogic.js
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Llamada a la función showLogin() después de que se ha cargado el contenido
-    showLogin();
-});
-
-// Function to show the login form and hide the create account form
+// Función para mostrar el formulario de inicio de sesión y ocultar el de crear cuenta
 function showLogin() {
     document.getElementById('loginForm').style.display = 'block';
     document.getElementById('createAccountForm').style.display = 'none';
-    setActiveTab(0); // Assuming the login tab is the first one
+    setActiveTab(0); // Asumiendo que el tab de inicio de sesión es el primero
 }
 
-// Function to show the create account form and hide the login form
+// Función para mostrar el formulario de crear cuenta y ocultar el de inicio de sesión
 function showCreateAccount() {
     document.getElementById('loginForm').style.display = 'none';
     document.getElementById('createAccountForm').style.display = 'block';
-    setActiveTab(1); // Assuming the create account tab is the second one
+    setActiveTab(1); // Asumiendo que el tab de crear cuenta es el segundo
 }
 
-// Function to set the active tab
+// Función para establecer el tab activo
 function setActiveTab(activeIndex) {
+    var tabs = document.querySelectorAll('.tab');
     tabs.forEach((tab, index) => {
         if (index === activeIndex) {
             tab.classList.add('active');
@@ -30,3 +25,27 @@ function setActiveTab(activeIndex) {
         }
     });
 }
+
+// Agregar event listener cuando el contenido se haya cargado
+document.addEventListener('DOMContentLoaded', function() {
+    // Mostrar el formulario de inicio de sesión por defecto
+    showLogin();
+
+    // Event listener para el botón de "Entrar"
+    var loginButton = document.querySelector('#loginForm button[type="submit"]');
+    loginButton.addEventListener('click', function(event) {
+        // Prevenir el comportamiento por defecto del formulario
+        event.preventDefault();
+        var emailInput = document.querySelector('#loginForm input[type="email"]').value;
+        var passwordInput = document.querySelector('#loginForm input[type="password"]').value;
+
+        // Verificar las credenciales
+        if (emailInput === '1' && passwordInput === '1') {
+            // Redirigir a historial.html si las credenciales son correctas
+            window.location.href = 'historial.html';
+        } else {
+            // Mostrar un mensaje de error si las credenciales son incorrectas
+            alert('Las credenciales son incorrectas. Por favor, intente de nuevo.');
+        }
+    });
+});
