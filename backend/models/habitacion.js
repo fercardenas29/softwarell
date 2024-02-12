63c06d32c992ef8bef16da07372bd25c2f7a6f3f
Mattew03 <mattew.guerrero@epn.edu.ec>
2024-02-12 18:05:12 -0500

    BD

--- backend/models/habitacion.js
+++ backend/models/habitacion.js
@@ -1,9 +1,11 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+
 var HabitacionSchema = Schema({
     nombre: String,
     precio: Number,
     descripcion: String,
-});
+}, { collection: 'habitaciones' }); // Aquí especificamos el nombre de la colección
 module.exports = mongoose.model('Habitacion', HabitacionSchema);
-//atributos habitaciones
+
+//var 
