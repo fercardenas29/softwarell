8a2d915186f5506fbe55e7546fc7066f16f029a5
Mattew03 <mattew.guerrero@epn.edu.ec>
2024-02-17 18:29:36 -0500

    Angular

---
+++ frontend2.0/src/app/components/hospedaje/crear-hospedaje/crear-hospedaje.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-hospedaje',
+  standalone: true,
+  imports: [],
+  templateUrl: './crear-hospedaje.component.html',
+  styleUrl: './crear-hospedaje.component.css'
+})
+export class CrearHospedajeComponent {
+
+}
