5dcb429803da480c69e72cf1f870e9532b65c579
eddyarias <eddy.arias@epn.edu.ec>
2024-02-20 11:36:00 -0500

    frontend with angular

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { HomeComponent } from './components/home/home.component';
 import { ContactoComponent } from './components/contacto/contacto.component'; 
 import { HospedajeComponent } from './components/hospedaje/hospedaje.component';
 import { LoginComponent } from './components/login/login.component';  
@@ -9,6 +9,7 @@ import { SesionComponent } from './components/sesion/sesion.component';
 import { SobreNosotrosComponent } from './components/sobre-nosotros/sobre-nosotros.component';
 
 const routes: Routes = [
+  {path:'inicio',component:HomeComponent},
   { path: 'contacto', component: ContactoComponent },
   { path: 'hospedaje', component: HospedajeComponent },
   { path: 'login', component: LoginComponent },
