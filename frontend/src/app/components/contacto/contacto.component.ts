c328989ec5011e655447998c0335e4d95cd73338
Mattew03 <mattew.guerrero@epn.edu.ec>
2024-02-22 09:09:18 -0500

    Optimizacion

--- frontend/src/app/components/contacto/contacto.component.ts
+++ frontend/src/app/components/contacto/contacto.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ContactoService } from '../../services/hotel.service';
 
 @Component({
   selector: 'app-contacto',
@@ -7,28 +6,5 @@ import { ContactoService } from '../../services/hotel.service';
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent {
-  nombre: string = '';
-  telefono: string = '';
-  correo: string = '';
-  mensaje: string = '';
 
-  constructor(private contactoService: ContactoService) {}
-
-  guardarContacto(contactoForm: any) {
-    if (contactoForm.valid) {
-      this.contactoService.guardarContacto(contactoForm.value)
-        .subscribe(
-          response => {
-            console.log('Contacto guardado exitosamente:', response);
-            // Aquí puedes realizar acciones adicionales después de guardar el contacto, como redireccionar a otra página o actualizar la lista de contactos.
-          },
-          error => {
-            console.error('Error al guardar contacto:', error);
-            // Aquí puedes manejar el error, mostrar un mensaje al usuario, etc.
-          }
-        );
-    } else {
-      console.error('Formulario inválido');
-    }
-  }
 }
